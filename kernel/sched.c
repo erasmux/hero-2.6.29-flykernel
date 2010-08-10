1e8161bac9ccc3a9776c9a7fe5b3177a88abf5cf
Eran <eranmizra@gmail.com>
2010-08-10 22:20:20 +0300

    Add interactive governor - does NOT boot

--- kernel/sched.c
+++ kernel/sched.c
@@ -2719,6 +2719,7 @@ unsigned long nr_running(void)
 
 	return sum;
 }
+EXPORT_SYMBOL_GPL(nr_running);
 
 unsigned long nr_uninterruptible(void)
 {
@@ -9723,3 +9724,5 @@ struct cgroup_subsys cpuacct_subsys = {
 	.subsys_id = cpuacct_subsys_id,
 };
 #endif	/* CONFIG_CGROUP_CPUACCT */
+
+
