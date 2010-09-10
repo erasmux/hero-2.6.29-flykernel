2c64467b673657ccf2c06672ac4bcb35de760357
Eran <eranmizra@gmail.com>
2010-09-10 17:09:06 +0300

    optimize rr_interval value

--- kernel/sched_bfs.c
+++ kernel/sched_bfs.c
@@ -116,7 +116,7 @@
  * Value is in ms and set to a minimum of 6ms. Scales with number of cpus.
  * Tunable via /proc interface.
  */
-int rr_interval __read_mostly = 6;
+int rr_interval __read_mostly = 3;
 
 /*
  * sched_iso_cpu - sysctl which determines the cpu percentage SCHED_ISO tasks
