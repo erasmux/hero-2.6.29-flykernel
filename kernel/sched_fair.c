0e89a16611814183a4a7226a8d95fd95bab16a00
Eran <eranmizra@gmail.com>
2010-12-22 20:32:14 +0200

    Optimize fair scheduler params to work well with HRTICK

--- kernel/sched_fair.c
+++ kernel/sched_fair.c
@@ -34,7 +34,7 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 20000000ULL;
+unsigned int sysctl_sched_latency = 60000000ULL;
 
 /*
  * Minimal preemption granularity for CPU-bound tasks:
